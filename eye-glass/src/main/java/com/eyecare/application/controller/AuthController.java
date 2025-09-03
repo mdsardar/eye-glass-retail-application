8e1e516a3e52a4b52611cb6871c330b2a45a6dbf
mdsardar <M5hammed1>
2025-09-03 16:33:40 +0530

    Included constructor injection

--- eye-glass/src/main/java/com/eyecare/application/controller/AuthController.java
+++ eye-glass/src/main/java/com/eyecare/application/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.eyecare.application.controller;
 import com.eyecare.application.dao.auth.dto.JwtAuthResponse;
 import com.eyecare.application.dao.auth.Login;
 import com.eyecare.application.service.authentication.AuthService;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +10,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<JwtAuthResponse> login(@RequestBody Login loginDto) {
